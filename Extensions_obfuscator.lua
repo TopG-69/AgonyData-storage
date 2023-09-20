010a3bcb6b58fbd1d70ddc37c3e8b00ea30139da
DAa <103658363+TopG-69@users.noreply.github.com>
2023-09-20 20:44:58 +0300

    Create Extensions_obfuscator.lua

---
+++ Extensions_obfuscator.lua
@@ -0,0 +1,130 @@
+Obfuscation_number = {
+    A = {315615, 178972, 026746, 591289, 462733, 895679, 487110, 109275, 674192, 920856, 305774, 712363, 936196, 428746, 560937},
+    B = {734179, 953602, 012032, 385249, 694012, 127680, 803462, 619547, 470228, 280559, 591414, 367279, 812940, 403668, 685192},
+    C = {857278, 783663, 927154, 645132, 729003, 138763, 506249, 491387, 870336, 365921, 214678, 789090, 120847, 536740, 402924},
+    D = {912578, 784589, 890072, 720613, 138450, 506483, 839745, 451706, 062429, 298576, 740913, 132046, 467150, 508668, 683745},
+    E = {012565, 673402, 002783, 836547, 912047, 470348, 518996, 926334, 479048, 348627, 271987, 605498, 982334, 347018, 186225},
+    F = {272578, 784512, 120046, 691407, 073450, 502783, 839056, 567181, 820631, 318794, 946530, 307281, 518967, 673024, 245656},
+    G = {216378, 786629, 029993, 503416, 719664, 648736, 362094, 947580, 801230, 305983, 627498, 983470, 701254, 548612, 129023},
+    H = {976337, 378702, 076189, 890764, 640275, 751234, 342051, 516748, 489270, 701548, 236748, 480971, 912430, 305681, 749847},
+    I = {126585, 852399, 993160, 609374, 742805, 051738, 380946, 462179, 798453, 530471, 716480, 804293, 935062, 621038, 387621},
+    J = {216226, 264388, 884173, 732942, 408506, 967184, 819951, 513049, 648237, 370132, 249873, 736048, 894202, 027189, 631469},
+    K = {976932, 328330, 128848, 470656, 582376, 760843, 432098, 981551, 516074, 743867, 672305, 051431, 314760, 609284, 847081},
+    L = {125918, 178612, 099923, 892706, 067354, 541072, 720439, 395826, 481626, 263030, 304947, 748558, 916357, 572683, 831014},
+    M = {265329, 293642, 178804, 048279, 793061, 614270, 703652, 528092, 921048, 487661, 615830, 302679, 795443, 431776, 760848},
+    N = {907660, 606488, 887761, 615992, 924783, 830461, 740236, 361898, 985014, 149226, 263445, 705801, 018785, 543927, 276007},
+    O = {012303, 038766, 662104, 049287, 173850, 605993, 937142, 428753, 503689, 896116, 247381, 810530, 769231, 314095, 952819},
+    P = {296359, 515230, 302842, 428576, 760109, 098774, 624037, 714950, 502638, 387131, 315289, 896774, 493067, 670538, 381472},
+    Q = {261262, 625795, 390209, 975048, 481670, 702563, 634780, 809192, 048531, 317695, 952861, 614025, 257018, 186497, 973052},
+    R = {257272, 045330, 309281, 810764, 640515, 152893, 938727, 274005, 763905, 051628, 927463, 630148, 849231, 314781, 810651},
+    S = {986554, 542649, 492109, 092631, 315487, 874363, 630271, 718614, 149582, 820627, 427896, 961207, 078653, 534130, 307237},
+    T = {216768, 681540, 400181, 810357, 574863, 631949, 497802, 023659, 598140, 402785, 381697, 976252, 520787, 874304, 041953},
+    U = {976276, 763624, 200130, 430961, 507867, 671498, 983112, 126785, 850269, 693487, 874630, 302501, 019453, 531664, 648764},
+    V = {065463, 638583, 831330, 307986, 860541, 412759, 594838, 387015, 159278, 780460, 602743, 430843, 749506, 061229, 298420},
+    W = {021793, 936483, 838481, 810447, 479651, 512763, 630849, 493276, 764582, 820190, 927654, 543168, 681940, 405717, 176222},
+    X = {843265, 655796, 965880, 807614, 142639, 398781, 617049, 493526, 267851, 518440, 409732, 324786, 869130, 305214, 140789},
+    Y = {967762, 625692, 923149, 610320, 204749, 387672, 530947, 479625, 854116, 167298, 983062, 621006, 408775, 759124, 243679},
+    Z = {132865, 656404, 045881, 810944, 438652, 529124, 247860, 603590, 715490, 902376, 846231, 316795, 957480, 807216, 160966}
+}
+
+Obfuscation_text = {
+    A = {'JgwtR', 'UwgsA', 'GBawK', 'syFzH', 'yXVtN', 'zJcnD', 'nJbwY', 'eWhvT', 'mYjuE', 'AwtXO', 'eKawZ', 'PuziT', 'cJpxG', 'yNuZb', 'mXtrX'},
+    B = {'KNrfE', 'JIwzC', 'NdfhU', 'NevxF', 'IzmcL', 'zNyBW', 'mLuXY', 'wusJT', 'jwrLB', 'FoykV', 'uLnXP', 'XapvG', 'VgBfe', 'ezRyI', 'zTpaS'},
+    C = {'JHQgX', 'BNWwZ', 'UIwqI', 'VblsW', 'GwxtZ', 'hPuNA', 'xNwaD', 'HhwCB', 'KwRpK', 'oKreC', 'KzCjX', 'fUlDH', 'RcWkX', 'XyzdY', 'LzNpT'},
+    D = {'OLYsF', 'ZHtyQ', 'qwGHR', 'NqArJ', 'wDcpN', 'CqLeU', 'qNfuK', 'NyTrG', 'kVylP', 'uGmzF', 'FwtdM', 'zGsRV', 'nXWuB', 'sMcjT', 'MzvBV'},
+    E = {'NygdW', 'OWyrM', 'NkdfN', 'FqZjy', 'JnVaW', 'nUZsV', 'IqLdM', 'kzRcN', 'MzGjy', 'tOuCo', 'qyjzq', 'wNpXb', 'Kfytz', 'mBhlF', 'pCxLk'},
+    F = {'kNgGD', 'BQqgR', 'BqyaN', 'Pjtnj', 'nYFpi', 'pGjRz', 'yvsmN', 'zNlgf', 'kEoFs', 'wHrYd', 'yRfni', 'UhwPu', 'rNdiM', 'lIyAx', 'fzqrs'},
+    G = {'oehWE', 'MnhdB', 'Buidf', 'lQjIr', 'sRzKo', 'tOsLs', 'vylcK', 'jyCtF', 'MzWbt', 'yTlsh', 'hDsCs', 'pTwfM', 'kAyru', 'Bzfuv', 'uYvJb'},
+    H = {'OJFwR', 'NdFgN', 'HirdF', 'vXgMz', 'gJwNo', 'oRxcT', 'mUjPF', 'zLdCA', 'yZpBR', 'rFhdi', 'yVpka', 'Gwjvu', 'NnTml', 'hHwiL', 'jBsyw'},
+    I = {'njdAk', 'Mkrdz', 'JHjsr', 'pCtJz', 'JZqyh', 'pDnBm', 'jDkxE', 'oPvFn', 'mEjCm', 'hNxAl', 'mRhCs', 'wDsHu', 'fzWdH', 'yVplD', 'kzYhq'},
+    J = {'nmnfX', 'NyawL', 'VqqqU', 'nEwXy', 'uIqyR', 'zOyUf', 'yUzjw', 'yKfjV', 'xRmFi', 'kVshz', 'sLhBl', 'yMlwX', 'iGzLw', 'lXsBt', 'jWmNm'},
+    K = {'HIwaR', 'NjurM', 'BusaD', 'wDzJh', 'pGpUt', 'xKoCd', 'zEzPt', 'wRdVF', 'pJhVf', 'fKjWk', 'kXoPk', 'bDyZr', 'lPcZj', 'rYiNv', 'vVpWp'},
+    L = {'NNFGX', 'ModhN', 'HuawC', 'xNjKu', 'xJpDm', 'bUyCu', 'vGjAp', 'zHxNu', 'pRzSj', 'jKuBs', 'mSdNj', 'xLpBk', 'jLyKw', 'wBtHs', 'sTfZi'},
+    M = {'ZCVRy', 'HapfE', 'NyhsD', 'yYxWq', 'uIhNo', 'bYkAr', 'cZfRj', 'xXoWz', 'qHvAr', 'hKwJd', 'eAqyF', 'dFgNo', 'jVzXz', 'zZpNr', 'zYkFz'},
+    N = {'BDFBu', 'HYWqf', 'BawgX', 'wXlTo', 'lOyKw', 'qFpTu', 'sHdMn', 'bUyPo', 'oYvNc', 'bZsAi', 'kFhYx', 'xXjyJ', 'oPdWr', 'tNwHb', 'nCzAt'},
+    O = {'VAWTR', 'NujDz', 'NidrA', 'yKvTk', 'kKwPu', 'zFgAd', 'eKjTk', 'cMgHv', 'vApBo', 'vMwFv', 'vRwHz', 'zWgAu', 'rUjZn', 'ePwLi', 'iRjWd'},
+    P = {'JHDOs', 'sUWhv', 'NhyaT', 'fUyTp', 'lHhBs', 'wLzFt', 'tUjHo', 'oHwFy', 'ySdKb', 'bHsPp', 'pOvZo', 'oKvPv', 'vKpUt', 'nAqya', 'xJkPl'},
+    Q = {'GGSEj', 'Kihdf', 'BofgU', 'oKyNr', 'qAqjo', 'mWvAo', 'wKmRv', 'vQsJy', 'yHhJh', 'hHxYk', 'ePdNc', 'jEjVr', 'vHqZz', 'zCkUe', 'eYzNf'},
+    R = {'NJHHr', 'kHuwL', 'NuisU', 'sZqXe', 'eYwXr', 'rJwBk', 'kZkPt', 'bMzFh', 'eLjTk', 'mPqFu', 'yGpFs', 'vGvWk', 'zFvRu', 'wKxWq', 'sUqLf'},
+    S = {'JBFDN', 'NzhfR', 'NHieO', 'tAqyi', 'kKmWt', 'jWgHi', 'mSdLu', 'xMzWx', 'xYmRp', 'kZkWw', 'wYmHp', 'pHhKm', 'nRkXw', 'lNwRk', 'nDzAf'},
+    T = {'NHBDi', 'Hwyhm', 'KpawL', 'cPwNp', 'xGvJb', 'tPjFs', 'pMjUk', 'kNpVb', 'bZrTp', 'vYpAj', 'ySvFw', 'wCwFn', 'fQzHt', 'tOvFh', 'hZmPk'},
+    U = {'Joawk', 'Pkmfu', 'Jpawz', 'kKvFm', 'mDpJz', 'zVgRf', 'xSdKn', 'nZgMp', 'rKqXn', 'nZwLj', 'jWdKk', 'eMzXr', 'rUjXv', 'vOxJb', 'bSdLn'},
+    V = {'hbsdf', 'fpJwq', 'Niawk', 'qKkHi', 'bSjNj', 'eKjMo', 'oXkYv', 'oNfTo', 'cVjKi', 'iXvTo', 'oUjRv', 'kHrXp', 'vLrZk', 'rBzHp', 'pXqHu'},
+    W = {'Wgyax', 'Nujzp', 'Ntdro', 'ePjTs', 'cRmPi', 'zYkZn', 'rPzFh', 'hLwNu', 'cXmNj', 'xGkPq', 'qBjMr', 'kZkUw', 'bQqHp', 'qJmYx', 'xLwTd'},
+    X = {'NxggR', 'PshwB', 'BAWhi', 'oJkPu', 'oNjTr', 'oUkPt', 'nWqTy', 'xOqHi', 'iHkPr', 'zBwTu', 'vKmPb', 'bDzFi', 'eRzHw', 'qTzHc', 'kPzHo'},
+    Y = {'YsgwD', 'ywHhX', 'Niuab', 'eMxNu', 'oWzNr', 'nDxKq', 'qFkXb', 'bSvFy', 'yRmZn', 'bLmZo', 'oLjMi', 'nBwHu', 'pQwHr', 'sKqZj', 'pFzHi'},
+    Z = {'Zhggz', 'MjdfX', 'MkldN', 'sPwNk', 'jHrXk', 'mWwNu', 'nMwFw', 'wZrPt', 'zNqHk', 'qKwXp', 'pGvTo', 'sMxHu', 'jFkZq', 'yGqHo', 'oQjZs'}
+}
+
+Random = function()
+	local RMath_Maxin = nil
+
+	local R_num1 = math.random(1, 10) R_num2 = math.random(1, 10)
+
+	if ((R_num1 - R_num2) == 5) then RMath_Maxin = 3
+		elseif ((R_num1 - R_num2) < 5 and (R_num1 - R_num2) > 0) then RMath_Maxin = 1
+		elseif ((R_num1 - R_num2) < 1) then RMath_Maxin = 2
+		elseif ((R_num1 - R_num2) > 5) then RMath_Maxin = 2
+	end
+
+	return RMath_Maxin
+end
+
+function ShuffleArray(Array)
+    for i = #Array, 2, -1 do
+        local j = math.random(i)
+        Array[i], Array[j] = Array[j], Array[i]
+    end
+end
+
+math.randomseed(os.time())
+Obfuscation_process = function(Table, Mode)
+	local Compleated = {}
+	for _, String in pairs(Table) do
+		if string.match(String, "[()%{%}_.,!%-? /;:]") then table.insert(Compleated, String) else
+		if (String ~= tonumber(String) and string.match(String, "[^%w]")) then if not string.match(String, "*") then return error("failed #0") end end
+	
+		if Mode == "obfuscate" then
+			if String ~= tonumber(String) then String = String:upper() end
+	
+			local NumberObf = Obfuscation_number[String] TextObf = Obfuscation_text[String]
+			local Method = Random() Obfuscation = nil
+	
+			if (NumberObf or TextObf) == nil then return error("failed #1") end
+			
+			if Method == 1 then Obfuscation = tostring(string.format("%s%d", TextObf[math.random(1, 5)], NumberObf[math.random(1, 5)]))
+				elseif Method == 2 then Obfuscation = tostring(string.format("%d%s", NumberObf[math.random(6, 10)], TextObf[math.random(6, 10)]))
+				elseif Method == 3 then Obfuscation = tostring(string.format("%s%d", TextObf[math.random(11, 15)], NumberObf[math.random(11, 15)]))
+			end
+			
+			local Numbers, NumbersTable = tonumber(string.match(Obfuscation, '%d+')), {} for Number in tostring(Numbers):gmatch(".") do table.insert(NumbersTable, Number) end
+			local Letters, LettersTable = string.gsub(Obfuscation, Numbers, ""), {} for Letter in Letters:gmatch(".") do table.insert(LettersTable, Letter) end ShuffleArray(LettersTable)
+
+			local Final_Obfuscation = ""
+			for i = 1, 6 do
+				if NumbersTable[i] then Final_Obfuscation = Final_Obfuscation .. NumbersTable[i] end
+				if LettersTable[i] then Final_Obfuscation = Final_Obfuscation .. LettersTable[i] end
+			end
+
+			if (Obfuscation or Final_Obfuscation) == nil then return error("failed #3") else table.insert(Compleated, Final_Obfuscation) end
+		elseif Mode == "unobfuscate" then
+			local Number = ""
+			local Progress = {} for i in tostring(String):gmatch(".") do if tonumber(i) then table.insert(Progress, i) end end
+			for i, v in pairs(Progress) do Number = Number .. v end
+	
+			for i, v in pairs(Obfuscation_number) do
+				for _, c in pairs(v) do
+					if string.match(c, Number) then
+						table.insert(Compleated, i)
+					end
+				end
+			end
+		end
+	end end if Compleated then return Compleated else return error("failed #6") end
+end
+
+
+
+
+
+
